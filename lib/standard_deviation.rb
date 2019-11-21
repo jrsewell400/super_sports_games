18d1e54e31fdb1b405fbd80f71f63317ea4606bf
Jordan Sewell <jrsewell400@gmail.com>
2019-11-21 13:51:49 -0700

    Test with a class or just a series of methods that all work together

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,9 +1,24 @@
-ages = [24, 30, 18, 20, 41]
-
 # Your code here for calculating the standard deviation
-
-output = ages.sum
-
-puts output
-
 # When you find the standard deviation, print it out
+# class StandardDeviation
+#
+# attr_reader :sum, :ages
+ages = [24, 30, 18, 20, 41]
+#
+#   def initialize(sum, ages)
+#     @sum = sum
+#     @ages = ages
+#   end
+
+  def output
+    sum = ages.sum
+  end
+
+addition = output
+puts sum
+# end
+
+# addition = StandardDeviation.new
+# addition.output
+# def number_of_intergers
+#   count =
