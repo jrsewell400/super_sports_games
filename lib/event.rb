0d04d9d72f99dc995a0462c06f9398ea83a9ebaf
Jordan Sewell <jrsewell400@gmail.com>
2019-11-21 15:57:06 -0700

    Add event.rb file and start the structure

---
+++ lib/event.rb
@@ -0,0 +1,22 @@
+class Event
+  attr_reader :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+
+  end
+
+  def max_age
+  end
+
+  def min_age
+  end
+
+  def average_age
+  end
+
+  def standard_deviation_age
+  end
+
+end
